e9958b0a0b684f227340ef67484d91da914ed641
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-07-29 10:04:51 +0530

    first commit

---
+++ src/main/java/com/health/metrics/HealthMonitorMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.health.metrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthMonitorMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthMonitorMetricsApplication.class, args);
+	}
+
+}
